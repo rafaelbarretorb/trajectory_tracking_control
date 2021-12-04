af4528ed16b7308097a476f4c86a1955dc6eafd4
rafaelbarretorb <rafaelbarretorb@gmail.com>
2021-12-03 21:53:46 -0300

    fix destructor implementation

--- include/trajectory_tracking_control/controller.hpp
+++ include/trajectory_tracking_control/controller.hpp
@@ -5,40 +5,42 @@
 #ifndef TRAJECTORY_TRACKING_CONTROL_CONTROLLER_HPP_
 #define TRAJECTORY_TRACKING_CONTROL_CONTROLLER_HPP_
 
+#include <vector>
 #include <utility>
+#include <string>
 
 namespace trajectory_tracking_control {
 
 class Controller {
  public:
-  virtual bool computeVelocityCommands(geometry_msgs::Twist& cmd_vel) = 0; //NOLINT
+  //virtual bool computeVelocityCommands(geometry_msgs::Twist& cmd_vel) = 0; //NOLINT
 
   virtual void loadControllerParams() = 0;
 
-  virtual void displayControllerInfo() = 0;
+  // virtual void displayControllerInfo() = 0;
 
-  virtual void updateReferenceState(double time) = 0;
+  // virtual void updateReferenceState(double time) = 0;
 
-  virtual bool isGoalReached() = 0;
+  // virtual bool isGoalReached() = 0;
 
-  virtual double getReferenceX() const = 0;
+  // virtual double getReferenceX() const = 0;
 
-  virtual double getReferenceY() const = 0;
+  // virtual double getReferenceY() const = 0;
 
-  virtual double getReferenceYaw() const = 0;
+  // virtual double getReferenceYaw() const = 0;
 
-  virtual double getReferenceLinearVelocity() const = 0;
+  // virtual double getReferenceLinearVelocity() const = 0;
 
-  virtual double getReferenceAngularVelocity() const = 0;
+  // virtual double getReferenceAngularVelocity() const = 0;
 
-  virtual void fillReferencePath(std::vector<std::pair<double, double>> *path) = 0;
+  // virtual void fillReferencePath(std::vector<std::pair<double, double>> *path) = 0;
 
 
-  virtual ~Controller();
+  virtual ~Controller() {}
 
-//  protected:
-//   Controller();
+ protected:
+  Controller() {}
 };
-}  // namespace 
+}  // namespace trajectory_tracking_control
 
 #endif  // TRAJECTORY_TRACKING_CONTROL_CONTROLLER_HPP_
